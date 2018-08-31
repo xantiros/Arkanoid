0fa7f6746593aa0aaa59dc6f9a5a4eb309c4d360
Xantiros <xantiros@gmail.com>
2018-08-31 15:50:46 +0200

    Timer

--- Arkanoid/MainWindow.xaml.cs
+++ Arkanoid/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,9 +21,31 @@ namespace Arkanoid
     /// </summary>
     public partial class MainWindow : Window
     {
+        int x = -8;
+        int y = -8;
+        Timer timer_ball = new Timer(100);
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //timer
+            timer_ball.Elapsed += Timer_ball_Elapsed;
+            timer_ball.Start();
+
+
+        }
+
+        private void Timer_ball_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                var margin = img_ball.Margin;
+                margin.Left += x;
+                margin.Top += y;
+                img_ball.Margin = margin;
+            });
+
         }
     }
 }
